6f17090384ebfe92098c614a4d91c83a707b95b7
Sinamon-maker <61106074+Sinamon-maker@users.noreply.github.com>
2021-06-15 22:57:46 +0300

    Initial commit

---
+++ client/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './main.scss';
+import Hom from './components/Hom';
+
+console.log(<Hom />)
+
+ReactDOM.render(<Hom />, document.getElementById('root'));
